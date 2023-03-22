148222e47ffccc2b4ffae3cc54299432d7915581
leeom <leeo.martins@outlook.com>
2023-03-22 19:46:40 -0300

    tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos (byte, shot, int, long, float, double, boolean, char)
+
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto1 = 1;
+        int numeroNormal = numeroCurto1;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
